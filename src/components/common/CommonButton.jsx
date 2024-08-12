d62626846e0c846f74a3cbf54df396fc048b57ec
Deepak-Maan <jattdevta1008@gmail.com>
2024-08-12 15:58:43 +0530

    modified navbar

--- src/components/common/CommonButton.jsx
+++ src/components/common/CommonButton.jsx
@@ -2,7 +2,7 @@ import { ButtonArrow } from "./Icons";
 
 const CommonButton = ({ buttonText, buttonClass, arrowColor }) => {
     return (
-        <button className={`text-white flex items-center group duration-300 bg-blue rounded-[48px] text-sm md:text-lg font-medium md:w-full font-saira py-1.5 md:py-3 mx-auto md:px-3 px-3 gap-[7px] h-10 leading-6 ${buttonClass}`}>
+        <button className={`text-white flex items-center group duration-300 bg-blue rounded-full text-sm md:text-lg font-medium md:w-full font-saira py-1.5 md:py-3 mx-auto md:px-3 px-3 gap-[7px] h-10 leading-6 ${buttonClass}`}>
             {buttonText}
             <span className="pt-0.5 group-hover:translate-x-1.5 duration-300">
                 <ButtonArrow color={arrowColor} />
